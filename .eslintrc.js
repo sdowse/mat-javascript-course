3189eaadbf88d5afcd3576c1128871b1cf83348e
student <test@test.com>
2019-03-04 15:46:14 +0000

    set up packages protractor, cusumber & chai

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:protractor/recommended",
+        "plugin:cucumber"
+    ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
