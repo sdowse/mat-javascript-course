538005680737dfded49172b1d4189eb680cb28bf
student <test@test.com>
2019-03-05 16:06:03 +0000

    added some page objects

---
+++ test/features/page_objects/add-product.page.js
@@ -0,0 +1,14 @@
+/**
+ * Page Object for the 'add product' page
+ * @constructor
+ */
+var AddProductPage = function(){
+
+    /**
+     * Elements on the page
+     */
+    this.productName =$('#mat-input-0');
+}
+
+};
+module.exports = new HomePage();
---
+++ test/features/page_objects/view-product.page.js
@@ -0,0 +1,8 @@
+/**
+ * Page object for view product / product detail
+ */
+var HomePage = function(){
+
+
+};
+module.exports = new HomePage();
